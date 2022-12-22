ff683980b1e53b6b6188df6df6ef7820af3c38b3
Mathia Pagani <98359675+bl000m@users.noreply.github.com>
2022-12-22 16:58:51 +0100

    header updated

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: mpagani <mpagani@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 18:26:03 by mpagani           #+#    #+#             */
-/*   Updated: 2022/12/20 14:04:55 by mpagani          ###   ########lyon.fr   */
+/*   Updated: 2022/12/22 16:54:29 by mpagani          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,34 @@
 # define FDF_H
 
 # include "../libft/libft.h"
-# include <fcntl.h> # to use open
+# include <fcntl.h>
 
-typedef struct s_values
+typedef struct s_fdf
 {
 	int				x;
 	int				y;
 	int				z;
-	struct s_values	*next;
-}	t_values;
+	struct s_fdf	*next;
+}	t_fdf;
+
+/* reading _ scanning map */
+t_fdf	**read_map(char *file_map);
+int		open_file_map(char *file_map);
+int		get_values(char *file_map, char c);
+int		get_x_max(int fd);
+int		get_y_max(int fd);
+
+/* checking */
+int		check_error(int fd);
+
+/* utils */
+void	add_node_back(t_fdf **node, t_fdf *new);
+t_fdf	*node_last(t_fdf *node);
+t_fdf	*new_node(int x, int y, int z);
+size_t	arr_len(char **str);
+
+/* free things */
+t_fdf	**allocate_values(int x_max, int y_max);
+void	free_all(t_fdf **result);
 
 #endif
